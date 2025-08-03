3e0c0f4af6bfdad0fa9a6debf093099c6dcd2a68
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 18:51:37 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/otakucraft/main.go
@@ -0,0 +1,23 @@
+// cmd/otakucraft/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"otakucraft/internal/otakucraft"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := otakucraft.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
